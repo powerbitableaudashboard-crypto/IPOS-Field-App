0751c776c92f7941833c2ec93d83a1ed15fad4bf
Benard <powerbitableaudashboard@gmail.com>
2026-08-09 23:07:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-
-});// iPOS Survey Service Worker
-var CACHE_NAME = "ipos-survey-v1";
-var urlsToCache = ["/index.html", "/manifest.json"];
+// iPOS Field App Service Worker
+var CACHE_NAME = "ipos-field-v2";
+var urlsToCache = ["./index.html", "./manifest.json"];
 
 self.addEventListener("install", function(e) {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(names.filter(function(n){ return n !== CACHE_NAME; }).map(function(n){ return caches.delete(n); }));
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", function(e) {
